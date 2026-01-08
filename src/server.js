ea82ee019d95701ce6fd4c2e21b745b1191b0755
PramodSolapur <psolapur5@gmail.com>
2026-01-08 13:11:29 +0530

    add linting

--- src/server.js
+++ src/server.js
@@ -5,18 +5,18 @@ import express from 'express';
 const app = express();
 
 app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK' });
+  res.status(200).json({ status: 'OK' });
 });
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'Welcome to Devops course',
-    });
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to Devops course',
+  });
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('Server is running on port: ', port);
+  console.log('Server is running on port: ', port);
 });
