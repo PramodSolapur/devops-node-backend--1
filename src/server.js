a9e0934273c45db719615a8ac52f1c4e8b1351d3
PramodSolapur <psolapur5@gmail.com>
2026-01-08 09:11:35 +0530

    deploy image to ec2 instance

--- src/server.js
+++ src/server.js
@@ -8,6 +8,13 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to Devops course",
+  });
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
